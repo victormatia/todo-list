a234f60261c8f57a32eba66380695a2b45e0dd28
Victor Matias <fvictor1705@gmail.com>
2022-05-30 20:34:23 -0300

    Resolve o requisito 7.

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
+function markTask() {
+  const wayToLi = document.querySelectorAll('li');
+
+  function mark(event) {
+    event.target.classList.add('itemMarked');
+  }
+
+  for (let itens of wayToLi) {
+    itens.addEventListener('click', mark);
+
+  }
+}
+
 function createNewTask() {
   const wayToButton = document.querySelector('#criar-tarefa');
   const wayToInput = document.querySelector('#texto-tarefa');
   const wayToOl = document.querySelector('#lista-tarefas')
 
   function addNewTask() {
-    console.log('clicado')
     const createNewLI = document.createElement('li');
 
     wayToOl.appendChild(createNewLI)
     createNewLI.innerText = wayToInput.value;
     wayToInput.value = '' // limpa o input
+    markTask();
   }
 
-
   wayToButton.addEventListener('click', addNewTask)
+
 }
 
 createNewTask();
+
+
