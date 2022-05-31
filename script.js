1d00c654e55108e8fcc9d8ea16edc6a07bb8df33
Victor Matias <fvictor1705@gmail.com>
2022-05-31 13:56:30 -0300

    Resolve requisito 10.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function markOffTask() {
   const wayToLi = document.querySelectorAll('li');
 
   for (let i = 0; i < wayToLi.length; i += 1) {
-    wayToLi[i].classList.remove('itemMarked');
+    wayToLi[i].classList.remove('item-marked');
   }
 }
 
@@ -11,7 +11,7 @@ function markOnTask() {
 
   function mark(event) {
     markOffTask()
-    event.target.classList.add('itemMarked');
+    event.target.classList.add('item-marked');
   }
 
   for (let itens of wayToLi) {
@@ -20,25 +20,61 @@ function markOnTask() {
   }
 }
 
-function createNewTask() {
-  const wayToButton = document.querySelector('#criar-tarefa');
-  const wayToInput = document.querySelector('#texto-tarefa');
-  const wayToOl = document.querySelector('#lista-tarefas')
+function completeTask() { // por algum motivo quando existem mais de 3 itens na lista os numeros impares não funcionam direito.
+  const wayToLi = document.querySelectorAll('li');
 
-  function addNewTask() {
-    const createNewLI = document.createElement('li');
+  function mark(event) {
+    const target = event.target;
 
-    wayToOl.appendChild(createNewLI)
-    createNewLI.innerText = wayToInput.value;
-    wayToInput.value = '' // limpa o input
-    markOnTask();
+    if (target.classList.contains('completed')) {
+      target.classList.remove('completed');
+    } else {
+      target.classList.add('completed');
+    }
   }
 
-  wayToButton.addEventListener('click', addNewTask)
+  for (let itens of wayToLi) {
+    itens.addEventListener('dblclick', mark);
+  }
+}
+
+function clearTasks () {
+  const wayToApagaTudo = document.querySelector('#apaga-tudo')
+  const wayToOl = document.querySelector('#lista-tarefas');
+  const wayToLi = document.querySelectorAll('li');
+
+  function deleteItem() {
+    for (let i =0; i < wayToLi.length; i += 1) {
+      wayToLi[i].remove();
+    }
+  }
+
+  wayToApagaTudo.addEventListener('click', deleteItem);
 
 }
 
+
+function createNewTask() {
+  const wayToButton = document.querySelector('#criar-tarefa');
+  const wayToInput = document.querySelector('#texto-tarefa');
+  const wayToOl = document.querySelector('#lista-tarefas');
+
+  function addNewTask() {
+    if (wayToInput.value == '') {
+      alert('Adicione um título para criar uma nova tarefa')
+      wayToInput.value = ''
+    } else {
+      const createNewLI = document.createElement('li');
+      wayToOl.appendChild(createNewLI)
+      createNewLI.innerText = wayToInput.value;
+      wayToInput.value = '' // limpa o input
+      markOnTask();
+      completeTask();
+      clearTasks();
+    }
+  }
+
+  wayToButton.addEventListener('click', addNewTask)
+}
+
 createNewTask();
-
-
-
